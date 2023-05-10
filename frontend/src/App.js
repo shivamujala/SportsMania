8d76b6918179c502623132a535b62ee42f0a7430
Armaankhan97 <pathaan72752488@gmail.com>
2023-05-10 16:31:42 +0530

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Main from "./components/main";
 import Home from "./components/main/Home";
 import Login from "./components/main/Login";
@@ -12,6 +12,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           
+          <Route path="/" element={<Navigate to="/main/home" />} />
           <Route path="main" element={<Main />} >
               <Route path="home" element={<Home />} />
               <Route path="login" element={<Login />} />
